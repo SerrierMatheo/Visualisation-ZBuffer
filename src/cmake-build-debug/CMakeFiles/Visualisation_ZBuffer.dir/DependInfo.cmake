e840c29498e8037389f795482a3c822012a50dcc
SerrierMatheo <matheoserrier@gmail.com>
2023-03-01 17:34:56 +0100

    test mac

--- src/cmake-build-debug/CMakeFiles/Visualisation_ZBuffer.dir/DependInfo.cmake
+++ src/cmake-build-debug/CMakeFiles/Visualisation_ZBuffer.dir/DependInfo.cmake
@@ -8,11 +8,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/serma/CLionProjects/Visualisation-ZBuffer/src/Face.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/Face.cpp.obj" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/Face.cpp.obj.d"
-  "C:/Users/serma/CLionProjects/Visualisation-ZBuffer/src/PlyObject3D.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/PlyObject3D.cpp.obj" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/PlyObject3D.cpp.obj.d"
-  "C:/Users/serma/CLionProjects/Visualisation-ZBuffer/src/Vertex.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/Vertex.cpp.obj" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/Vertex.cpp.obj.d"
-  "C:/Users/serma/CLionProjects/Visualisation-ZBuffer/src/main.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/main.cpp.obj" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/main.cpp.obj.d"
-  "C:/Users/serma/CLionProjects/Visualisation-ZBuffer/src/test/test.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/test/test.cpp.obj" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/test/test.cpp.obj.d"
+  "/Users/stages/Documents/Matheo/Visualisation-ZBuffer/src/Face.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/Face.cpp.o" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/Face.cpp.o.d"
+  "/Users/stages/Documents/Matheo/Visualisation-ZBuffer/src/PlyObject3D.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/PlyObject3D.cpp.o" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/PlyObject3D.cpp.o.d"
+  "/Users/stages/Documents/Matheo/Visualisation-ZBuffer/src/Vertex.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/Vertex.cpp.o" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/Vertex.cpp.o.d"
+  "/Users/stages/Documents/Matheo/Visualisation-ZBuffer/src/main.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/main.cpp.o" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/main.cpp.o.d"
+  "/Users/stages/Documents/Matheo/Visualisation-ZBuffer/src/test/test.cpp" "CMakeFiles/Visualisation_ZBuffer.dir/test/test.cpp.o" "gcc" "CMakeFiles/Visualisation_ZBuffer.dir/test/test.cpp.o.d"
   )
 
 # Targets to which this target links.
--- src/cmake-build-debug/cmake_install.cmake
+++ src/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/serma/CLionProjects/Visualisation-ZBuffer/src
+# Install script for directory: /Users/stages/Documents/Matheo/Visualisation-ZBuffer/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Visualisation_ZBuffer")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/serma/CLionProjects/Visualisation-ZBuffer/src/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/stages/Documents/Matheo/Visualisation-ZBuffer/src/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
